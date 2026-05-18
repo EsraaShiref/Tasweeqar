0963787066b0bb60e9514b44c8f8a25ef66ea4cf
Esraa Shiref <israashiref@gmail.com>
2026-05-19 02:49:34 +0300

    feat: implement reusable navbar and footer components with localized design

--- src/app/shared/components/navbar.ts
+++ src/app/shared/components/navbar.ts
@@ -1,4 +1,5 @@
-import { Component, HostListener, signal, inject } from '@angular/core';
+import { Component, HostListener, signal, inject, OnInit, OnDestroy, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { ThemeService } from '../../core/services/theme';
@@ -11,12 +12,31 @@ import { LanguageService } from '../../core/services/language';
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
-export class Navbar {
+export class Navbar implements OnInit, OnDestroy {
   protected themeService = inject(ThemeService);
   protected langService = inject(LanguageService);
+  private platformId = inject(PLATFORM_ID);
 
   menuOpen = signal(false);
   scrolled = false;
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      window.addEventListener('scroll', this.closeOnScroll.bind(this));
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      window.removeEventListener('scroll', this.closeOnScroll.bind(this));
+    }
+  }
+
+  private closeOnScroll(): void {
+    if (this.menuOpen()) {
+      this.menuOpen.set(false);
+    }
+  }
 
   @HostListener('window:scroll')
   onScroll() {
